a4a61da8b2d0c30771c0f8e4e15ecb676cf97af5
Yurii Mysochenko <yuriy.mysochenko@gmail.com>
2018-04-24 20:24:19 +0300

    remove useless method call

--- app/src/main/java/com/sdex/activityrunner/MainActivity.java
+++ app/src/main/java/com/sdex/activityrunner/MainActivity.java
@@ -210,12 +210,6 @@ public class MainActivity extends BaseActivity {
   }
 
   @Override
-  protected void onResume() {
-    super.onResume();
-    isProVersionEnabled = appPreferences.isProVersion();
-  }
-
-  @Override
   public void onBackPressed() {
     super.onBackPressed();
     adsDelegate.showInterstitial();
