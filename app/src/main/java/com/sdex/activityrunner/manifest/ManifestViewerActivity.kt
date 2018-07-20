c6921f4c7180f4a670a9dfc7eb247b369100de12
Yurii Mysochenko <yuriy.mysochenko@gmail.com>
2018-07-20 20:24:57 +0300

    add manifest doc link

--- app/src/main/java/com/sdex/activityrunner/manifest/ManifestViewerActivity.kt
+++ app/src/main/java/com/sdex/activityrunner/manifest/ManifestViewerActivity.kt
@@ -4,7 +4,12 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
+import android.support.v4.content.ContextCompat
+import android.view.Menu
+import android.view.MenuItem
 import com.pddstudio.highlightjs.models.Language
 import com.pddstudio.highlightjs.models.Theme
 import com.sdex.activityrunner.R
@@ -45,6 +50,26 @@ class ManifestViewerActivity : BaseActivity() {
       .loadManifest(packageName).observe(this, Observer {
         highlightView.setSource(it)
       })
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.manifest_viewer, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.itemId) {
+      R.id.action_help -> {
+        val url = "https://developer.android.com/guide/topics/manifest/manifest-intro"
+        val builder = CustomTabsIntent.Builder()
+        builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        builder.setShowTitle(true)
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(url))
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
+    }
   }
 
   companion object {
