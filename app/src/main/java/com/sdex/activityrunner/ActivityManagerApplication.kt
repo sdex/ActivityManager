292cedf5e4d6a5c7f0d0039f577869e1bdc19eb4
Yurii Mysochenko <yuriy.mysochenko@gmail.com>
2018-12-27 20:30:13 +0200

    disable multidex

--- app/src/main/java/com/sdex/activityrunner/ActivityManagerApplication.kt
+++ app/src/main/java/com/sdex/activityrunner/ActivityManagerApplication.kt
@@ -1,8 +1,6 @@
 package com.sdex.activityrunner
 
 import android.app.Application
-import android.content.Context
-import android.support.multidex.MultiDex
 import android.util.Log
 import com.flurry.android.FlurryAgent
 
@@ -10,27 +8,11 @@ class ActivityManagerApplication : Application() {
 
   override fun onCreate() {
     super.onCreate()
-//    if (BuildConfig.DEBUG) {
-//      StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
-//        .detectAll()
-//        .penaltyLog()
-//        .build())
-//      StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
-//        .detectAll()
-//        .penaltyLog()
-//        .build())
-//    }
-
     FlurryAgent.Builder()
       .withIncludeBackgroundSessionsInMetrics(false)
-      .withCaptureUncaughtExceptions(true)
+      .withCaptureUncaughtExceptions(!BuildConfig.DEBUG)
       .withLogEnabled(BuildConfig.DEBUG)
       .withLogLevel(Log.VERBOSE)
       .build(this, "HMVT99FMRW22SXSSRQRQ")
-  }
-
-  override fun attachBaseContext(base: Context) {
-    super.attachBaseContext(base)
-    MultiDex.install(this)
   }
 }
