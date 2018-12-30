2c5db78b2d3f56bcf86611e642da0be562210618
Yurii Mysochenko <yuriy.mysochenko@gmail.com>
2018-12-30 16:47:32 +0200

    disable analytics for debug builds

--- app/src/main/java/com/sdex/commons/analytics/AnalyticsManager.kt
+++ app/src/main/java/com/sdex/commons/analytics/AnalyticsManager.kt
@@ -1,37 +1,44 @@
 package com.sdex.commons.analytics
 
 import com.flurry.android.FlurryAgent
+import com.sdex.activityrunner.BuildConfig
 import com.sdex.activityrunner.app.ActivityModel
 import com.sdex.activityrunner.db.cache.ApplicationModel
 
 object AnalyticsManager {
 
   fun logApplicationOpen(model: ApplicationModel) {
-    FlurryAgent.logEvent("application_open", mapOf(
-      "package_name" to model.packageName,
-      "app_name" to model.name
-    ))
+    if (!BuildConfig.DEBUG) {
+      FlurryAgent.logEvent("application_open", mapOf(
+        "package_name" to model.packageName,
+        "app_name" to model.name
+      ))
+    }
   }
 
   fun logActivityOpen(model: ActivityModel,
                       withParams: Boolean = false,
                       withRoot: Boolean = false) {
-    FlurryAgent.logEvent("activity_open", mapOf(
-      "package_name" to model.packageName,
-      "class_name" to model.className,
-      "name" to model.name,
-      "exported" to model.exported.toString(),
-      "with_params" to withParams.toString(),
-      "with_root" to withRoot.toString()
-    ))
+    if (!BuildConfig.DEBUG) {
+      FlurryAgent.logEvent("activity_open", mapOf(
+        "package_name" to model.packageName,
+        "class_name" to model.className,
+        "name" to model.name,
+        "exported" to model.exported.toString(),
+        "with_params" to withParams.toString(),
+        "with_root" to withRoot.toString()
+      ))
+    }
   }
 
   fun logCreateShortcut(model: ActivityModel) {
-    FlurryAgent.logEvent("create_shortcut", mapOf(
-      "package_name" to model.packageName,
-      "class_name" to model.className,
-      "name" to model.name,
-      "exported" to model.exported.toString()
-    ))
+    if (!BuildConfig.DEBUG) {
+      FlurryAgent.logEvent("create_shortcut", mapOf(
+        "package_name" to model.packageName,
+        "class_name" to model.className,
+        "name" to model.name,
+        "exported" to model.exported.toString()
+      ))
+    }
   }
 }
