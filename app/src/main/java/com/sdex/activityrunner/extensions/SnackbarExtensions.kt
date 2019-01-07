991a9546eb63cd4c4b6d5b06a04a94fa5c149295
Yurii Mysochenko <yuriy.mysochenko@gmail.com>
2019-01-07 19:54:22 +0200

    temporary disable snackbar padding

--- app/src/main/java/com/sdex/activityrunner/extensions/SnackbarExtensions.kt
+++ app/src/main/java/com/sdex/activityrunner/extensions/SnackbarExtensions.kt
@@ -1,18 +1,15 @@
 package com.sdex.activityrunner.extensions
 
-import android.content.res.Resources
-import android.view.ViewGroup
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
 import com.google.android.material.snackbar.Snackbar
 import com.sdex.activityrunner.R
 
 fun Snackbar.config(margin: Int = 12, elevation: Float = 6f) {
   setActionTextColor(ContextCompat.getColor(context, R.color.yellow))
-  val marginPx = (margin * Resources.getSystem().displayMetrics.density).toInt()
-  val params = view.layoutParams as ViewGroup.MarginLayoutParams
-  params.setMargins(marginPx, marginPx, marginPx, marginPx)
-  view.layoutParams = params
-  view.setBackgroundResource(R.drawable.background_snackbar)
-  ViewCompat.setElevation(view, elevation)
+//  val marginPx = (margin * Resources.getSystem().displayMetrics.density).toInt()
+//  val params = view.layoutParams as ViewGroup.MarginLayoutParams
+//  params.setMargins(marginPx, marginPx, marginPx, marginPx)
+//  view.layoutParams = params
+//  view.setBackgroundResource(R.drawable.background_snackbar)
+//  ViewCompat.setElevation(view, elevation)
 }
