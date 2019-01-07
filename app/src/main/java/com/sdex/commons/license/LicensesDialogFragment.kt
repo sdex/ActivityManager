ccdef428e1e39ee297e4af3ba1fed5d33ebbd828
Yurii Mysochenko <yuriy.mysochenko@gmail.com>
2019-01-07 13:26:45 +0200

    update licenses

---
+++ app/src/main/java/com/sdex/commons/license/LicensesDialogFragment.kt
@@ -0,0 +1,48 @@
+package com.sdex.commons.license
+
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Message
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import androidx.appcompat.app.AlertDialog
+import com.sdex.commons.BaseDialogFragment
+
+class LicensesDialogFragment : BaseDialogFragment() {
+
+  override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+    val webView = createWebView(requireContext())
+    webView.loadUrl("file:///android_asset/licenses.html")
+    return AlertDialog.Builder(requireContext())
+      .setTitle("Notices")
+      .setView(webView)
+      .setPositiveButton("Close", null)
+      .create()
+  }
+
+  private fun createWebView(context: Context): WebView {
+    val webView = WebView(context)
+    webView.settings.setSupportMultipleWindows(true)
+    webView.webChromeClient = object : WebChromeClient() {
+      override fun onCreateWindow(view: WebView, isDialog: Boolean,
+                                  isUserGesture: Boolean, resultMsg: Message): Boolean {
+        val result = view.hitTestResult
+        val data = result.extra
+        if (data != null) {
+          val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(data))
+          context.startActivity(browserIntent)
+        }
+        return false
+      }
+    }
+    return webView
+  }
+
+  companion object {
+
+    const val TAG = "LicensesDialogFragment"
+  }
+}
