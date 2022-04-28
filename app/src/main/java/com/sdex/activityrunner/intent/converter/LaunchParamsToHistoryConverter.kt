15a8ee8238b1c4326a176f7d72a0124a766eba09
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2022-04-28 11:55:57 +0300

    refactoring

--- app/src/main/java/com/sdex/activityrunner/intent/converter/LaunchParamsToHistoryConverter.kt
+++ app/src/main/java/com/sdex/activityrunner/intent/converter/LaunchParamsToHistoryConverter.kt
@@ -3,22 +3,23 @@ package com.sdex.activityrunner.intent.converter
 import com.sdex.activityrunner.db.history.HistoryModel
 import com.sdex.activityrunner.intent.LaunchParams
 
-class LaunchParamsToHistoryConverter(private val launchParams: LaunchParams) :
-    Converter<HistoryModel> {
+class LaunchParamsToHistoryConverter(
+    private val launchParams: LaunchParams
+) : Converter<HistoryModel> {
 
     override fun convert(): HistoryModel {
         val integerListSerializer = IntegerListSerializer()
         val extrasSerializer = ExtrasSerializer()
-        val historyModel = HistoryModel()
-        historyModel.timestamp = System.currentTimeMillis()
-        historyModel.packageName = launchParams.packageName
-        historyModel.className = launchParams.className
-        historyModel.action = launchParams.action
-        historyModel.data = launchParams.data
-        historyModel.mimeType = launchParams.mimeType
-        historyModel.categories = integerListSerializer.serialize(launchParams.categories)
-        historyModel.flags = integerListSerializer.serialize(launchParams.flags)
-        historyModel.extras = extrasSerializer.serialize(launchParams.extras)
-        return historyModel
+        return HistoryModel().apply {
+            timestamp = System.currentTimeMillis()
+            packageName = launchParams.packageName
+            className = launchParams.className
+            action = launchParams.action
+            data = launchParams.data
+            mimeType = launchParams.mimeType
+            categories = integerListSerializer.serialize(launchParams.categories)
+            flags = integerListSerializer.serialize(launchParams.flags)
+            extras = extrasSerializer.serialize(launchParams.extras)
+        }
     }
 }
