336c0b386ca0c3e78ee47f3a6df927981c9aee73
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2022-11-18 10:49:16 +0200

    use new package manager flags on android T

--- app/src/main/java/com/sdex/activityrunner/util/BetterPackageManager.kt
+++ app/src/main/java/com/sdex/activityrunner/util/BetterPackageManager.kt
@@ -5,41 +5,83 @@ import android.content.Intent
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
+import android.os.Build
 import java.io.File
 
+@Suppress("DEPRECATION")
 fun getComponentIcon(pm: PackageManager, componentName: ComponentName): Drawable {
     return try {
         val intent = Intent()
         intent.component = componentName
-        val resolveInfo = pm.resolveActivity(intent, 0)
+        val resolveInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pm.resolveActivity(intent, PackageManager.ResolveInfoFlags.of(0))
+        } else {
+            pm.resolveActivity(intent, 0)
+        }
         resolveInfo?.loadIcon(pm) ?: pm.defaultActivityIcon
     } catch (e: Exception) {
         pm.defaultActivityIcon
     }
 }
 
+@Suppress("DEPRECATION")
 fun getInstalledPackages(pm: PackageManager): List<String> {
-    return pm.getInstalledPackages(0)
+    val packages = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        pm.getInstalledPackages(PackageManager.PackageInfoFlags.of(0))
+    } else {
+        pm.getInstalledPackages(0)
+    }
+    val intent = Intent(Intent.ACTION_MAIN)
+    return packages
         .map { it.packageName }
         .ifEmpty {
-            pm.queryIntentActivities(Intent(Intent.ACTION_MAIN), 0)
-                .map { it.activityInfo.applicationInfo.packageName }
+            val intentActivities = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                pm.queryIntentActivities(intent, PackageManager.ResolveInfoFlags.of(0))
+            } else {
+                pm.queryIntentActivities(intent, 0)
+            }
+            intentActivities.map { it.activityInfo.applicationInfo.packageName }
         }
 }
 
+@Suppress("DEPRECATION")
 fun getPackageInfo(pm: PackageManager, packageName: String): PackageInfo {
     return try {
-        pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pm.getPackageInfo(
+                packageName, PackageManager.PackageInfoFlags.of(
+                    PackageManager.GET_ACTIVITIES.toLong()
+                )
+            )
+        } else {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
+        }
     } catch (e: Exception) {
         getApkPackageInfo(pm, packageName)
     }
 }
 
+@Suppress("DEPRECATION")
 private fun getApkPackageInfo(pm: PackageManager, packageName: String): PackageInfo {
     try {
-        val info = pm.getPackageInfo(packageName, PackageManager.GET_META_DATA)
+        val info = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pm.getPackageInfo(
+                packageName, PackageManager.PackageInfoFlags.of(
+                    PackageManager.GET_META_DATA.toLong()
+                )
+            )
+        } else {
+            pm.getPackageInfo(packageName, PackageManager.GET_META_DATA)
+        }
         val file = File(info.applicationInfo.publicSourceDir)
-        val archiveInfo = pm.getPackageArchiveInfo(file.absolutePath, PackageManager.GET_ACTIVITIES)
+        val archiveInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pm.getPackageArchiveInfo(
+                file.absolutePath,
+                PackageManager.PackageInfoFlags.of(PackageManager.GET_ACTIVITIES.toLong())
+            )
+        } else {
+            pm.getPackageArchiveInfo(file.absolutePath, PackageManager.GET_ACTIVITIES)
+        }
         info.activities = archiveInfo?.activities
         return info
     } catch (e: Exception) {
