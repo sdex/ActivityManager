7d8e7895f4cb13813066e912999871018b53bdaf
Yurii Poudanien <mcpchip@gmail.com>
2023-04-11 20:39:04 +0300

    * removed unused import

--- app/src/main/java/com/sdex/activityrunner/manifest/ManifestViewerActivity.kt
+++ app/src/main/java/com/sdex/activityrunner/manifest/ManifestViewerActivity.kt
@@ -7,7 +7,6 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
