aab77509a7286a7ed2e9e6fa4ea35e35427ff4f3
Jesse205 <2055675594@qq.com>
2023-08-28 22:25:25 +0800

    fix: app bar lift color while scrolling

--- app/src/main/java/com/sdex/activityrunner/app/AppBarLayoutBehavior.kt
+++ app/src/main/java/com/sdex/activityrunner/app/AppBarLayoutBehavior.kt
@@ -6,6 +6,10 @@ import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.appbar.AppBarLayout
 
+/**
+ * Fix no change in app bar color after scrolling view slide to top on Android 12+ devices.
+ * @author Jesse205
+ * */
 class AppBarLayoutBehavior(context: Context?, attrs: AttributeSet?) :
     AppBarLayout.Behavior(context, attrs) {
     override fun onNestedScroll(
@@ -30,6 +34,7 @@ class AppBarLayoutBehavior(context: Context?, attrs: AttributeSet?) :
             type,
             consumed
         )
+        // Fix no change in app bar color after scrolling view slide to top on Android 12+ devices.
         if (child.isLiftOnScroll) {
             child.isLifted = dyUnconsumed >= 0
         }
