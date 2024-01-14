3b3d0d3a5e8deb8501d28fe14c368fcc46ee8753
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2024-01-14 13:32:01 +0100

    add filter dialog

---
+++ app/src/main/java/com/sdex/activityrunner/app/dialog/FilterBottomSheetDialogFragment.kt
@@ -0,0 +1,118 @@
+package com.sdex.activityrunner.app.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.sdex.activityrunner.MainActivity
+import com.sdex.activityrunner.databinding.DialogFilterBinding
+import com.sdex.activityrunner.db.cache.ApplicationModel
+import com.sdex.activityrunner.db.cache.query.GetApplicationsQuery
+import com.sdex.activityrunner.preferences.AppPreferences
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class FilterBottomSheetDialogFragment : BottomSheetDialogFragment() {
+
+    @Inject
+    lateinit var appPreferences: AppPreferences
+    private var _binding: DialogFilterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogFilterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        when (appPreferences.sortBy) {
+            ApplicationModel.NAME -> binding.sortByName.isChecked = true
+            ApplicationModel.UPDATE_TIME -> binding.sortByUpdateTime.isChecked = true
+            ApplicationModel.INSTALL_TIME -> binding.sortByInstallTime.isChecked = true
+        }
+        when (appPreferences.sortOrder) {
+            GetApplicationsQuery.ASC -> binding.orderByAsc.isChecked = true
+            GetApplicationsQuery.DESC -> binding.orderByDesc.isChecked = true
+        }
+        binding.showSystemApps.isChecked = appPreferences.isShowSystemApps
+        binding.showSystemAppIndicator.isEnabled = appPreferences.isShowSystemApps
+        binding.showSystemAppIndicator.isChecked = appPreferences.isShowSystemAppIndicator
+        binding.showDisabledApps.isChecked = appPreferences.isShowDisabledApps
+        binding.showDisabledAppIndicator.isEnabled = appPreferences.isShowDisabledApps
+        binding.showDisabledAppIndicator.isChecked = appPreferences.isShowDisabledAppIndicator
+
+        binding.sortByName.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                appPreferences.sortBy = ApplicationModel.NAME
+                refresh()
+            }
+        }
+        binding.sortByUpdateTime.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                appPreferences.sortBy = ApplicationModel.UPDATE_TIME
+                refresh()
+            }
+        }
+        binding.sortByInstallTime.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                appPreferences.sortBy = ApplicationModel.INSTALL_TIME
+                refresh()
+            }
+        }
+        binding.orderByAsc.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                appPreferences.sortOrder = GetApplicationsQuery.ASC
+                refresh()
+            }
+        }
+        binding.orderByDesc.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                appPreferences.sortOrder = GetApplicationsQuery.DESC
+                refresh()
+            }
+        }
+        binding.showSystemApps.setOnCheckedChangeListener { _, isChecked ->
+            appPreferences.isShowSystemApps = isChecked
+            binding.showSystemAppIndicator.isEnabled = isChecked
+            refresh()
+        }
+        binding.showSystemAppIndicator.setOnCheckedChangeListener { _, isChecked ->
+            appPreferences.isShowSystemAppIndicator = isChecked
+            update()
+        }
+        binding.showDisabledApps.setOnCheckedChangeListener { _, isChecked ->
+            appPreferences.isShowDisabledApps = isChecked
+            binding.showDisabledAppIndicator.isEnabled = isChecked
+            refresh()
+        }
+        binding.showDisabledAppIndicator.setOnCheckedChangeListener { _, isChecked ->
+            appPreferences.isShowDisabledAppIndicator = isChecked
+            update()
+        }
+    }
+
+    private fun refresh() {
+        (activity as MainActivity?)?.refresh()
+    }
+
+    private fun update() {
+        (activity as MainActivity?)?.update()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+
+        const val TAG = "FilterBottomSheetDialogFragment"
+    }
+}
--- app/src/main/java/com/sdex/activityrunner/preferences/AppPreferences.kt
+++ app/src/main/java/com/sdex/activityrunner/preferences/AppPreferences.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
+import com.sdex.activityrunner.db.cache.ApplicationModel
+import com.sdex.activityrunner.db.cache.query.GetApplicationsQuery
 
 class AppPreferences(context: Context) {
 
@@ -12,16 +14,11 @@ class AppPreferences(context: Context) {
 
     var isNotExportedDialogShown: Boolean
         get() = preferences.getBoolean(KEY_NOT_EXPORTED_DIALOG_SHOWN, false)
-        set(value) = preferences.edit()
-            .putBoolean(KEY_NOT_EXPORTED_DIALOG_SHOWN, value)
-            .apply()
+        set(value) = preferences.edit {
+            putBoolean(KEY_NOT_EXPORTED_DIALOG_SHOWN, value)
+        }
     val appOpenCounter: Int
         get() = preferences.getInt(KEY_OPEN_APP_COUNTER, 0)
-    var showLineNumbers: Boolean
-        get() = preferences.getBoolean(KEY_SHOW_LINE_NUMBERS, true)
-        set(value) = preferences.edit()
-            .putBoolean(KEY_SHOW_LINE_NUMBERS, value)
-            .apply()
 
     fun onAppOpened() {
         preferences.edit {
@@ -31,30 +28,72 @@ class AppPreferences(context: Context) {
 
     /* user preferences */
 
-    val isShowSystemApps: Boolean
+    var isShowSystemApps: Boolean
         get() = userPreferences.getBoolean(KEY_SHOW_SYSTEM_APPS, true)
+        set(value) {
+            userPreferences.edit {
+                putBoolean(KEY_SHOW_SYSTEM_APPS, value)
+            }
+        }
 
-    val isShowSystemAppIndicator: Boolean
+    var isShowSystemAppIndicator: Boolean
         get() = userPreferences.getBoolean(KEY_SHOW_SYSTEM_APP_LABEL, false)
+        set(value) {
+            userPreferences.edit {
+                putBoolean(KEY_SHOW_SYSTEM_APP_LABEL, value)
+            }
+        }
 
-    val isShowDisabledApps: Boolean
+    var isShowDisabledApps: Boolean
         get() = userPreferences.getBoolean(KEY_SHOW_DISABLED_APPS, true)
+        set(value) {
+            userPreferences.edit {
+                putBoolean(KEY_SHOW_DISABLED_APPS, value)
+            }
+        }
 
-    val isShowDisabledAppIndicator: Boolean
+    var isShowDisabledAppIndicator: Boolean
         get() = userPreferences.getBoolean(KEY_SHOW_DISABLED_APP_LABEL, false)
+        set(value) {
+            userPreferences.edit {
+                putBoolean(KEY_SHOW_DISABLED_APP_LABEL, value)
+            }
+        }
 
     var showNotExported: Boolean
         get() = userPreferences.getBoolean(KEY_SHOW_NOT_EXPORTED, false)
         set(value) {
-            userPreferences.edit()
-                .putBoolean(KEY_SHOW_NOT_EXPORTED, value)
-                .apply()
+            userPreferences.edit {
+                putBoolean(KEY_SHOW_NOT_EXPORTED, value)
+            }
+        }
+
+    var showLineNumbers: Boolean
+        get() = preferences.getBoolean(KEY_SHOW_LINE_NUMBERS, true)
+        set(value) = preferences.edit {
+            putBoolean(KEY_SHOW_LINE_NUMBERS, value)
         }
 
     @AppCompatDelegate.NightMode
     val theme: Int
         get() = userPreferences.getString(KEY_THEME, null)?.toInt()
             ?: AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+
+    var sortBy: String
+        get() = userPreferences.getString(KEY_SORT_BY, ApplicationModel.NAME)!!
+        set(value) {
+            userPreferences.edit {
+                putString(KEY_SORT_BY, value)
+            }
+        }
+
+    var sortOrder: String
+        get() = userPreferences.getString(KEY_ORDER_BY, GetApplicationsQuery.ASC)!!
+        set(value) {
+            userPreferences.edit {
+                putString(KEY_ORDER_BY, value)
+            }
+        }
 
     companion object {
 
@@ -69,5 +108,8 @@ class AppPreferences(context: Context) {
         const val KEY_SHOW_DISABLED_APPS = "show_disabled_apps"
         private const val KEY_SHOW_DISABLED_APP_LABEL = "advanced_disabled_app"
         const val KEY_THEME = "appearance_theme"
+
+        const val KEY_SORT_BY = "sort_by"
+        const val KEY_ORDER_BY = "order_by"
     }
 }
