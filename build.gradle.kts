d46552d827b524e143abaa3e698b443ca4704931
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2024-04-02 08:49:26 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.android.application") version "8.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
     id("androidx.room") version "2.6.1" apply false
 }
