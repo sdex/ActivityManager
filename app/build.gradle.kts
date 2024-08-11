eb68f7a6a10ea61a92f72673c9577d335837e219
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2024-08-11 07:11:01 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,23 +82,23 @@ room {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-ktx:1.9.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.browser:browser:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.2")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     val roomVersion: String by rootProject.extra
     implementation("androidx.room:room-ktx:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.vectordrawable:vectordrawable:1.1.0")
+    implementation("androidx.vectordrawable:vectordrawable:1.2.0")
     val glideVersion = "4.16.0"
     implementation("com.github.bumptech.glide:glide:$glideVersion")
     ksp("com.github.bumptech.glide:ksp:$glideVersion")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     val hiltVersion: String by rootProject.extra
     implementation ("com.google.dagger:hilt-android:$hiltVersion") {
         exclude(group = "androidx.fragment", module = "fragment")
@@ -110,14 +110,14 @@ dependencies {
     implementation("com.simplecityapps:recyclerview-fastscroll:2.0.1")
     implementation("com.tomergoldst.android:tooltips:1.1.1")
     implementation("net.dongliu:apk-parser:2.6.10")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("com.github.Y-E-P:BrowserFiP:1.0.5")
 
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation("androidx.room:room-testing:$roomVersion")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test:rules:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.6.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 buildscript {
-    val hiltVersion by extra("2.51") // update the plugin version as well
+    val hiltVersion by extra("2.52") // update the plugin version as well
     val roomVersion by extra("2.6.1") // update the plugin version as well
 }
 
 plugins {
-    id("com.android.application") version "8.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
     id("androidx.room") version "2.6.1" apply false
 }
