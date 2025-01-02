6fc14e37fbdaf0d3a309c1a59303b9e1a442ac48
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2025-01-02 08:34:37 +0100

    update gradle and agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
