0e0235d54c98abca0aabbb79725ed42ed4553450
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2025-12-02 08:51:31 +0100

    log shortcut manager info

--- app/src/main/java/com/sdex/activityrunner/shortcut/ShortcutManager.kt
+++ app/src/main/java/com/sdex/activityrunner/shortcut/ShortcutManager.kt
@@ -7,6 +7,7 @@ import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.core.content.pm.ShortcutManagerCompat
 import androidx.core.graphics.drawable.IconCompat
 import com.sdex.activityrunner.R
+import timber.log.Timber
 
 fun createShortcut(context: Context, name: String, intent: Intent, icon: Bitmap?): Boolean {
     val iconCompat = if (icon != null) {
@@ -18,13 +19,22 @@ fun createShortcut(context: Context, name: String, intent: Intent, icon: Bitmap?
 }
 
 fun createShortcut(context: Context, name: String, intent: Intent, icon: IconCompat): Boolean {
+    val isRateLimitingActive = ShortcutManagerCompat.isRateLimitingActive(context)
     if (ShortcutManagerCompat.isRequestPinShortcutSupported(context)) {
         val pinShortcutInfo = ShortcutInfoCompat.Builder(context, name)
             .setIcon(icon)
             .setShortLabel(name)
             .setIntent(intent)
             .build()
-        return ShortcutManagerCompat.requestPinShortcut(context, pinShortcutInfo, null)
+        val requestPinShortcutResult = ShortcutManagerCompat.requestPinShortcut(
+            context,
+            pinShortcutInfo,
+            null,
+        )
+        Timber.d("Pin shortcut: isRateLimitingActive=$isRateLimitingActive, " +
+            "requestPinShortcutResult=$requestPinShortcutResult")
+        return requestPinShortcutResult
     }
+    Timber.d("Pin shortcut is not supported")
     return false
 }
