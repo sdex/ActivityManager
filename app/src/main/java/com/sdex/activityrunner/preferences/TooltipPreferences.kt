97559df8b25dddcdad6966923efe3c3f2dc22b4e
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2025-12-28 15:19:52 +0100

    Refactor TooltipPreferences constructor

--- app/src/main/java/com/sdex/activityrunner/preferences/TooltipPreferences.kt
+++ app/src/main/java/com/sdex/activityrunner/preferences/TooltipPreferences.kt
@@ -1,17 +1,14 @@
 package com.sdex.activityrunner.preferences
 
 import android.content.Context
-import android.content.SharedPreferences
 import androidx.core.content.edit
-import kotlin.properties.Delegates
 
 class TooltipPreferences(context: Context) {
 
-    private var sharedPreferences: SharedPreferences by Delegates.notNull()
-
-    init {
-        sharedPreferences = context.getSharedPreferences("tooltips_states", Context.MODE_PRIVATE)
-    }
+    private val sharedPreferences = context.getSharedPreferences(
+        "tooltips_states",
+        Context.MODE_PRIVATE,
+    )
 
     var showChangeIcon: Boolean
         get() = sharedPreferences.getBoolean(SHORTCUT_ICON_CHANGE, SHORTCUT_ICON_CHANGE_DEFAULT)
